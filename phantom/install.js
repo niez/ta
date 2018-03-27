317d0bb7f5d474463671674179dfc2c8b73552e9
nick <ziyang.nie@qq.com>
2018-03-27 23:26:48 +0800

    add code in phantom folder

---
+++ phantom/install.js
@@ -0,0 +1,18 @@
+const kew = require('kew');
+
+function checkPhantomInLib () {
+    return kew.fcall(function () {
+        return findPhantomjsInLib()
+    });
+}
+
+
+
+kew.resolve(true)
+.then(checkPhantomjsInLib)
+.then(checkPhantomjsOnPath)
+.then(downloadPhantomjs)
+.then(extractZip)
+.then(function (result) {
+    console.log('Path is :' + result);
+})
